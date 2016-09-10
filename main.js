fda40a320c1d7f6abf70887817e885ebf0e847bc
Scott Harvey <scottyharvey33@gmil.com>
2016-09-10 12:50:05 -0600

    branch check

--- main.js
+++ main.js
@@ -63,3 +63,30 @@ var romanNum = "XXVI";
 
 romanConv(romanNum);
 //
+
+//////////////////////////
+//A square of squares/////
+
+// You like building blocks.
+ // You especially like building blocks that are squares.
+ // And what you even like more, is to arrange them into a square of square building blocks!
+//
+// However, sometimes, you can't arrange them into a square.
+// Instead, you end up with an ordinary rectangle! Those blasted things!
+// If you just had a way to know, whether you're currently working in vain… Wait! That's it!
+// You just have to check if your number of building blocks is a perfect square.
+//ANSWER
+var isSquare = function(n){
+	var square = Math.sqrt(n);
+	var rounded = Math.floor(square);
+	if((rounded * rounded) === n ){
+
+		return true
+	}
+  else{
+  	return false;
+  };
+}
+
+isSquare(79);
+//
