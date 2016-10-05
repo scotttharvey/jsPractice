8e43259c4ae0e4a26cc7ddd779866aba46416342
Scott Harvey <scottyharvey33@gmil.com>
2016-10-05 13:50:40 -0600

    Toy Problem

--- main.js
+++ main.js
@@ -143,3 +143,16 @@ function isPangram(string){
    return /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string)
 }
 ///
+/////Sum of all the multiples of 3 or 5///
+function findSum(n) {
+var sum = 0;
+for (var i = 0; i <= n; i++)
+{
+    if (i % 3 === 0 || i % 5 === 0)
+    {
+       sum += i;
+    }
+}
+  return sum;
+}
+/////
